7bb6c1e041a4369d70b28735fe0b11964e0b0717
Yahya <user@Yahyas-MacBook-Pro.local>
2025-11-23 06:44:24 +0000

    Update message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {"message": "🚀 Deployment Success — Yahya API Auto Updated!"}
+    return {"message": "🔥 Yahya API Auto Deploy Test WORKS!"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
