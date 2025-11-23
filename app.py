be5a7384f4bba1954416b5f433be1f0a8c073c6f
YHaider19 <yahya.haider2002@gmail.com>
2025-11-23 05:25:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message":"Yahya’s CI/CD is working"}"})
+    return jsonify({"message": "Hello from Yahya’s API V2!"}"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
